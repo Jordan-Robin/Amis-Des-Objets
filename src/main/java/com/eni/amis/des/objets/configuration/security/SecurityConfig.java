9c64515a61e4369818eb8af6571b1b4bf63857c0
Jérémy Champigny <jeremy.champigny2024@campus-eni.fr>
2024-11-27 09:13:50 -0400

    test

--- src/main/java/com/eni/amis/des/objets/configuration/security/SecurityConfig.java
+++ src/main/java/com/eni/amis/des/objets/configuration/security/SecurityConfig.java
@@ -8,8 +8,8 @@ import org.springframework.security.provisioning.UserDetailsManager;
 
 import javax.sql.DataSource;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
 
     private static final String SELECT_USER = "SELECT pseudo, mot_de_passe AS password, 1 FROM UTILISATEURS WHERE " +
@@ -17,7 +17,7 @@ public class SecurityConfig {
     private static final String SELECT_ROLE = "SELECT u.email, r.ROLE from UTILISATEURS as u INNER JOIN ROLES as r " +
             "ON u.administrateur = r.IS_ADMIN WHERE u.pseudo=?";
 
-    @Bean
+    //@Bean
     UserDetailsManager userDetailsManager(DataSource dataSource) {
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
         jdbcUserDetailsManager.setUsersByUsernameQuery(SELECT_USER);
