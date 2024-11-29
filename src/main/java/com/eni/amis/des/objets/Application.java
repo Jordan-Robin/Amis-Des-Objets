a98e5e69dfaa12939c877e013fa3667cbd176e99
JordanRobin <jordanrobin100@gmail.com>
2024-11-29 15:14:34 +0100

    clé d'externalisation pour l'internationalisation

--- src/main/java/com/eni/amis/des/objets/Application.java
+++ src/main/java/com/eni/amis/des/objets/Application.java
@@ -1,5 +1,6 @@
 package com.eni.amis.des.objets;
 
+import com.eni.amis.des.objets.exceptions.BusinessCode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println(BusinessCode.FRAG_HEADER_A_ACCUEIL);
 	}
 
 }
