287735fa7fa7100ba1935a0bac95a3ae6d186424
JordanRobin <jordanrobin100@gmail.com>
2024-12-02 10:25:59 +0100

    récupération main

--- src/main/java/com/eni/amis/des/objets/controllers/UserController.java
+++ src/main/java/com/eni/amis/des/objets/controllers/UserController.java
@@ -3,10 +3,8 @@ package com.eni.amis.des.objets.controllers;
 import com.eni.amis.des.objets.bll.UserServices;
 import com.eni.amis.des.objets.bo.Adresse;
 import com.eni.amis.des.objets.bo.Utilisateur;
-import com.eni.amis.des.objets.dal.AddressDAOImpl;
 import com.eni.amis.des.objets.exceptions.BusinessCode;
 import com.eni.amis.des.objets.exceptions.BusinessException;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,8 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Controller
 public class UserController {
